21f90593cdd8cbc8077740533d85d35e6c1b19af
dkGithup2022 <104286091+dkGithup2022@users.noreply.github.com>
2023-10-15 00:38:28 +0900

    3장까지 책내용 요약

---
+++ src/test/java/com/dev/wiki/cleanarchitecture/AppTest.java
@@ -0,0 +1,14 @@
+package com.dev.wiki.cleanarchitecture;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AppTest {
+
+	@Test
+	public void HelloTest(){
+		System.out.println("Hello Test~");
+	}
+
+}
